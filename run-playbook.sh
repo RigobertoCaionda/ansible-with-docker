7a3e7d1ceea8b56e529a98e7ae771e0551982e87
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2026-02-04 14:07:54 +0100

    creating the ansible.cfg file

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-ansible-playbook -i hosts --vault-pass-file=.vault_pass playbook.yaml
+#ansible-playbook -i hosts --vault-pass-file=.vault_pass playbook.yaml
+# Como temos o arquivo ansible.cfg, podemos simplificar o comando acima, sem precisar passar o inventário:
+ansible-playbook --vault-pass-file=.vault_pass playbook.yaml
